dc6af531c2c09745ce1c2afffa8d052557be91c2
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 02:32:11 +0900

    環境構築

--- centos-7-6-18-10-config-env.sh
+++ centos-7-6-18-10-config-env.sh
@@ -3,3 +3,5 @@ echo 'export LD_LIBRARY_PATH=/usr/local/lib' >>~/.bashrc
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 echo 'export JAVA_HOME=/usr/local/src/jdk-11' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/src/go'>>~/.bashrc
+echo 'export PATH=$GOROOT/bin:$PATH'>>~/.bashrc
