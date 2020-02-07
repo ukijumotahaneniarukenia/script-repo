99f70fe17598485abbd1e2ee60b2b111c8b30c3f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 01:50:47 +0900

    環境構築

--- centos-7-6-18-10-config-env.sh
+++ centos-7-6-18-10-config-env.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo 'export LD_LIBRARY_PATH=/usr/local/lib' >>~/.bashrc
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
+echo 'export JAVA_HOME=/usr/local/src/jdk-11' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
---
+++ centos-7-6-18-10-install-jdk13.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /usr/local/src && \
+curl -LO https://download.java.net/java/GA/jdk13.0.1/cec27d702aa74d5a8630c65ae61e4305/9/GPL/openjdk-13.0.1_linux-x64_bin.tar.gz && \
+tar xvf openjdk-13*_bin.tar.gz
