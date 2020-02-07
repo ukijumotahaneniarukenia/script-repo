9d12907e3b68e63d77aba69b271064730815523f
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 00:30:35 +0900

    環境構築

---
+++ centos-7-6-18-10-install-git.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+yum install -y \
+gcc \
+autoconf \
+curl-devel \
+expat-devel \
+gettext-devel \
+openssl-devel \
+perl-devel \
+zlib-devel \
+perl-ExtUtils-MakeMaker \
+asciidoc \
+xmlto \
+docbook2X
+
+ln -fns /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi && \
+cd /usr/local/src && \
+curl -LkvOf https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.24.1.tar.gz && \
+tar xvf git-2.24.1.tar.gz && \
+cd git-2.24.1 && \
+make -j12 prefix=/usr/local all && \
+make -j12 prefix=/usr/local install
