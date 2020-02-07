03190abb7763f7c462502bfc2bbefd923ae0f3ad
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 00:01:27 +0900

    環境構築

---
+++ centos-7-6-18-10-install-dev-pkg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+yum install -y \
+bzip2-devel \
+gdbm-devel \
+libbz2-dev \
+libffi-devel \
+libffi-devpandoc \
+libreadline-dev \
+libsqlite3-dev \
+libssl-dev \
+libuuid-devel \
+ncurses-devel \
+openssl-devel \
+readline-devel \
+sqlite-devel \
+tk-devel \
+xz-devel \
+zlib-devel
