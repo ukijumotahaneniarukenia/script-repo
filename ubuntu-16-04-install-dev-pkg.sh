55c05f02c2849678812b166de326af09533a6391
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-09 00:33:29 +0900

    環境構築

---
+++ ubuntu-16-04-install-dev-pkg.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+apt install -y \
+bzip2-devel \
+gdbm-devel \
+libbz2-dev \
+libffi-dev \
+libffi-devel \
+libffi-devpandoc \
+libgdbm-dev \
+libgl1-mesa-dev \
+libncurses5-dev \
+libnss3-dev \
+libreadline-dev \
+libreadline6-dev \
+libsqlite3-dev \
+libssl-dev \
+libuuid-devel \
+ncurses-devel \
+openssl-devel \
+readline-devel \
+sqlite-devel \
+tk-devel \
+xz-devel \
+zlib-devel \
+zlib1g-dev
