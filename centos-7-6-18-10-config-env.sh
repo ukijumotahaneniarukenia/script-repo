b69147400c0284ef96f7f43ff67a7723b45568f8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 14:26:48 +0900

    環境構築

--- centos-7-6-18-10-config-env.sh
+++ centos-7-6-18-10-config-env.sh
@@ -15,7 +15,9 @@ echo 'export PATH=$JETTY_HOME/bin:$PATH' >> ~/.bashrc
 echo 'export PATH=$STS_HOME:$PATH' >> ~/.bashrc
 echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bashrc
 echo 'export PERL5LIB=/home/perl/perl5/lib/perl5:/home/perl/perl5/lib/perl5/x86_64-linux-thread-multi' >> ~/.bashrc
+echo 'export PATH=/usr/local/src/Komodo-Edit-11/bin:$PATH' >> ~/.bashrc
 
 echo 'alias pyc="bash /usr/local/src/pycharm-community-2019.3.1/bin/pycharm.sh 1>~/launch-pycharm.log 2>&1 &"' >>~/.bashrc
 echo 'alias idea="/usr/local/src/idea-IC-192.7142.36/bin/idea.sh >~/launch-idea.log 2>&1 &"' >> ~/.bashrc
-echo 'alias myb="mysql-workbench 1>~/launch_mysql-bench.log 2>&1 &"' >>~/.bashrc
+echo 'alias myb="mysql-workbench 1>~/launch-mysql-bench.log 2>&1 &"' >>~/.bashrc
+echo 'alias komo="komodo --verbose 1>~/launch-komodo.log 2>&1 &"' >> ~/.bashrc
---
+++ centos-7-6-18-10-install-komodo-11-1-1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /usr/local/src && \
+tar -xzf Komodo-Edit-11.1.1-18206-linux-x86_64.tar.gz
