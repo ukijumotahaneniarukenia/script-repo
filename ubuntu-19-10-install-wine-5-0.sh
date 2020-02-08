91990072ef443114c1131721433310e4fa8b5f4d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 22:07:24 +0900

    環境構築

---
+++ ubuntu-19-10-install-wine-5-0.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+apt install -y wget gnupg gnupg2 gnupg1
+dpkg --add-architecture i386
+cd /opt && wget -nc https://dl.winehq.org/wine-builds/winehq.key; apt-key add winehq.key
+apt install -y software-properties-common
+apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ eoan main'
+#以下のコマンドを実行した時、Enter入力を求められる
+add-apt-repository ppa:cybermax-dexter/sdl2-backport
+apt update
+apt install -y --install-recommends winehq-stable
