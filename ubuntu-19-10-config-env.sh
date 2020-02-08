86abebbb6079f5424d8334c40930d44957aac06c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 22:40:52 +0900

    環境構築

--- ubuntu-19-10-config-env.sh
+++ ubuntu-19-10-config-env.sh
@@ -2,6 +2,10 @@
 echo 'export LD_LIBRARY_PATH=/usr/local/lib' >>~/.bashrc
 echo 'export JAVA_HOME=/usr/local/src/jdk-11' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export MAVEN_HOME=/usr/local/src/apache-maven-3.6.3'>>~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export GRADLE_HOME=/usr/local/src/gradle-6.1'>>~/.bashrc
+echo 'export PATH=$GRADLE_HOME/bin:$PATH' >> ~/.bashrc
 
 echo 'alias pyc="bash /usr/local/src/pycharm-community-2019.3.1/bin/pycharm.sh 1>~/launch_pycharm.log 2>&1 &"' >>~/.bashrc
 echo 'alias qtd="( mkdir -p $HOME/qt-wrksp && export XDG_RUNTIME_DIR=$HOME/qt-wrksp && cd /usr/lib/x86_64-linux-gnu/qt5/bin && ./designer 1>~/launch_qt_designer.log 2>&1 </dev/null & )"' >>~/.bashrc
---
+++ ubuntu-19-10-install-gradle-6-1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /usr/local/src && \
+unzip gradle-6.1-all.zip
