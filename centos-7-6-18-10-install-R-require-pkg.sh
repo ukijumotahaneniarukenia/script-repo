ddb9faea41046b95f42c5a9358dc8e8bbd161489
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 15:36:50 +0900

    環境構築

---
+++ centos-7-6-18-10-install-R-require-pkg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+yum install -y \
+libcurl-devel \
+openssl-devel \
+libxml2-devel \
+mysql-devel \
+udunits2-devel \
+libsodium-devel \
+ImageMagick-c++-devel \
+poppler-cpp-devel
