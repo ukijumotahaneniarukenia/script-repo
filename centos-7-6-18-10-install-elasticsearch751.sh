9a8624fa9a03256e4761e8abab590d9ccb838e3c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 12:26:23 +0900

    環境構築

--- centos-7-6-18-10-install-elasticsearch751.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /usr/local/src && \
-curl -LO https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-x86_64.rpm && \
-yum install -y elasticsearch-7.5.1-x86_64.rpm
