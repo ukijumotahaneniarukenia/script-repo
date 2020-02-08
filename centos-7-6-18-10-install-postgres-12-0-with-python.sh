6d8143a323ca3e1f811e501159314bc4bf0d823b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-08 16:52:15 +0900

    環境構築

--- centos-7-6-18-10-install-postgres-12-0-with-python.sh
+++ centos-7-6-18-10-install-postgres-12-0-with-python.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
+yum install -y python-devel
+
 cd /usr/local/src && \
 curl -LO https://ftp.postgresql.org/pub/source/v12.0/postgresql-12.0.tar.gz && \
 tar xvf postgresql-12.0.tar.gz && \
 chown -R root:root postgresql-12.0 && \
 cd postgresql-12.0 && \
-./configure --prefix=/usr/local --libdir=/usr/local/lib/postgresql --with-perl && \
+./configure --prefix=/usr/local --libdir=/usr/local/lib/postgresql --with-python && \
 make -j12 && \
 make -j12 install
