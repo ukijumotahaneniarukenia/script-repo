786be620109af5db6bb8d679821f4694009dcafb
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 22:50:01 +0900

    スケッチ

--- a.sh
+++ a.sh
@@ -5,7 +5,6 @@ src="$1"
 [ -z "$src" ] && exit 1
 
 while read tgt;do
-
-  echo "cp "$src" "$tgt
-
+  [ "$src" == "$tgt" ] || :
+  [ "$src" != "$tgt" ] && echo "cp "$src" "$tgt
 done < <(ls /usr/local/src/script-repo | grep -Po '[a-z]+(-[0-9]{1,}){1,}' | grep -P 'ubuntu|centos' | sort | uniq | xargs -I@ echo "@$(echo "$src" | grep -Po '\-install.*')")
---
+++ centos-7-6-18-10-install-xml2json.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src && \
+git clone https://github.com/hay/xml2json.git
