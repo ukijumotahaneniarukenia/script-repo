aa20045f29731c2ca5d321680bbeee33f249b334
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 14:23:46 +0900

    れぽ

--- centos-7-6-18-10-install-mecab-ipadic-2-7-0.sh
+++ centos-7-6-18-10-install-mecab-ipadic-2-7-0.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd /usr/local/src && \
 wget -O mecab-ipadic-2.7.0-20070801.tar.gz "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM" && \
 tar -xzf mecab-ipadic-2.7.0-20070801.tar.gz && \
 cd mecab-ipadic-2.7.0-20070801 && \
