3003c0ba87f0e6dbb978da747fb29a0076d8c69c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 11:25:36 +0900

    環境構築

--- ubuntu-19-10-config-env.sh
+++ ubuntu-19-10-config-env.sh
@@ -10,5 +10,7 @@ echo 'export GOROOT=/usr/local/src/go'>>~/.bashrc
 echo 'export PATH=$GOROOT/bin:$PATH'>>~/.bashrc
 echo 'export SWIFT_HOME=/usr/local/src/swift-DEVELOPMENT-SNAPSHOT-2020-01-31-a-ubuntu18.04/usr' >>~/.bashrc
 echo 'export PATH=$SWIFT_HOME/bin:$PATH' >>~/.bashrc
+echo 'export APACHE_SOLR_HOME=/usr/local/src/solr-8.4.1' >>~/.bashrc
+echo 'export PATH=$APACHE_SOLR_HOME/bin:$PATH' >>~/.bashrc
 echo 'alias pyc="bash /usr/local/src/pycharm-community-2019.3.1/bin/pycharm.sh 1>~/launch_pycharm.log 2>&1 &"' >>~/.bashrc
 echo 'alias qtd="( mkdir -p $HOME/qt-wrksp && export XDG_RUNTIME_DIR=$HOME/qt-wrksp && cd /usr/lib/x86_64-linux-gnu/qt5/bin && ./designer 1>~/launch_qt_designer.log 2>&1 </dev/null & )"' >>~/.bashrc
