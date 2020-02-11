ea7a86e9faac3235a1c9cd02c97ed49504cdace0
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-11 14:37:38 +0900

    れぽ

--- ubuntu-16-04-config-dotfile.sh
+++ ubuntu-16-04-config-dotfile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd $HOME && git clone https://github.com/ukijumotahaneniarukenia/dotfile.git
-find $HOME/dotfile -type f -name "*" | grep -vP '\.git|README.md' | perl -pe 's;.*/;;g' | xargs -I@ echo "cp $HOME/dotfile/@ $HOME/@" | sh
+find $HOME/dotfile -type f -name "*" | grep -vP '.git/|README.md' | perl -pe 's;.*/;;g' | xargs -I@ echo "cp $HOME/dotfile/@ $HOME/@" | sh
