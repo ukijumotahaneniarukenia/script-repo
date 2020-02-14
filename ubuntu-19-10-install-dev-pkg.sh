9f63734a9fcffbd593403290641650b4fa25f297
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-14 23:41:20 +0900

    環境構築

--- ubuntu-19-10-install-dev-pkg.sh
+++ ubuntu-19-10-install-dev-pkg.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 apt install -y \
-bzip2-devel \
-gdbm-devel \
 libbz2-dev \
 libffi-dev \
-libffi-devel \
-libffi-devpandoc \
 libgdbm-dev \
 libgl1-mesa-dev \
 libncurses5-dev \
@@ -14,12 +10,4 @@ libreadline-dev \
 libreadline6-dev \
 libsqlite3-dev \
 libssl-dev \
-libuuid-devel \
-ncurses-devel \
-openssl-devel \
-readline-devel \
-sqlite-devel \
-tk-devel \
-xz-devel \
-zlib-devel \
 zlib1g-dev
