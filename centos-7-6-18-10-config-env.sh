875406e1ccbd5b23920cb51d2362bcd275a4cd50
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-24 21:59:07 +0900

    repo

--- centos-7-6-18-10-config-env.sh
+++ centos-7-6-18-10-config-env.sh
@@ -4,6 +4,7 @@
 mkdir -p $HOME/.local/bin
 echo 'export PATH=$HOME/.local/bin:$PATH' >>$HOME/.bashrc
 echo 'export PATH=/usr/local/bin:$PATH' >>$HOME/.bashrc
+echo 'export PATH=$HOME/.nimble/bin:$PATH' >>~/.bashrc
 
 echo 'export ELASTICSEARCH_HOME=/usr/share/elasticsearch'>>$HOME/.bashrc
 echo 'export PATH=$ELASTICSEARCH_HOME/bin:$PATH'>>$HOME/.bashrc
