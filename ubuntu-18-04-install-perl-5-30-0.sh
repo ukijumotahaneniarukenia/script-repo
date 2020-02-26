cd7afedeeb50a13207749a3350d954f6315bb29b
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-02-26 21:28:14 +0900

    Create ubuntu-18-04-install-perl-5-30-0.sh

---
+++ ubuntu-18-04-install-perl-5-30-0.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /usr/local/src && \
+curl -LO https://www.cpan.org/src/5.0/perl-5.30.0.tar.gz && \
+tar -xzf perl-5.30.0.tar.gz && \
+cd perl-5.30.0 && \
+./Configure -des -Dprefix=/usr/local -Duseshrplib && \
+make -j12 && \
+make -j12 install
