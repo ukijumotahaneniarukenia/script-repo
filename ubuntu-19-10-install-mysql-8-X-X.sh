bf11aef689a9fe975ad9e46e5e63efdd4199d7c4
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-02-28 00:25:36 +0900

    Update ubuntu-19-10-install-mysql-8-X-X.sh

--- ubuntu-19-10-install-mysql-8-X-X.sh
+++ ubuntu-19-10-install-mysql-8-X-X.sh
@@ -4,4 +4,4 @@ cd /usr/local/src && \
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb && \
 dpkg -i mysql-apt-config_0.8.15-1_all.deb && \
 apt update && \
-apt install -y mysql-server && \
+apt install -y mysql-server
