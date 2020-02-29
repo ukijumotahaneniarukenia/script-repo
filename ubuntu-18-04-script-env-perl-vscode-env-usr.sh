e4758bddfb25b59c09cf63c12acc2c7bbb944462
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-29 19:31:22 +0900

    環境構築

--- ubuntu-18-04-script-env-perl-vscode-env-usr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-groupadd -g 1001 kuraine && \
-useradd -m -g kuraine -u 1001 kuraine && \
-chsh -s /bin/bash kuraine && \
-echo 'kuraine:kuraine_pwd' | chpasswd && \
-echo 'kuraine ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
-echo 'root:root_pwd' | chpasswd
