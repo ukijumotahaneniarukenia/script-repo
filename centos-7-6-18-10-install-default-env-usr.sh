52afe87540182b1b27f9937301e761d1dad7daf9
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-02-29 23:32:32 +0900

    環境構築

--- centos-7-6-18-10-install-default-env-usr.sh
+++ centos-7-6-18-10-install-default-env-usr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-groupadd -g 1001 kuraine && \
-useradd -m -g kuraine -u 1001 kuraine && \
+groupadd -g 9999 kuraine && \
+useradd -m -g kuraine -u 9999 kuraine && \
 chsh -s /bin/bash kuraine && \
 echo 'kuraine:kuraine_pwd' | chpasswd && \
 echo 'kuraine ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
