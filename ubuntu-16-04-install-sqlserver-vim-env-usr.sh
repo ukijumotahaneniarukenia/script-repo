09aafc3fe3ae76718e05e90aeec7b657c6000f9c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-01 14:32:23 +0900

    環境構築

--- ubuntu-16-04-install-sqlserver-vim-env-usr.sh
+++ ubuntu-16-04-install-sqlserver-vim-env-usr.sh
@@ -5,9 +5,4 @@ useradd -m -g mssql -u 1001 mssql && \
 chsh -s /bin/bash mssql && \
 echo 'mssql:mssql_pwd' | chpasswd && \
 echo 'mssql ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-groupadd -g 9999 kuraine && \
-useradd -m -g kuraine -u 9999 kuraine && \
-chsh -s /bin/bash kuraine && \
-echo 'kuraine:kuraine_pwd' | chpasswd && \
-echo 'kuraine ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 echo 'root:root_pwd' | chpasswd
