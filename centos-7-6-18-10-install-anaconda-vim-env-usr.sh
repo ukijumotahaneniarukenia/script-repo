98a3a86ba00ae452b643d40afefa514e5e0798e0
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-04 23:21:00 +0900

    環境構築

--- centos-7-6-18-10-install-anaconda-vim-env-usr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-groupadd -g 1001 anacon && \
-useradd -m -g anacon -u 1001 anacon && \
-chsh -s /bin/bash anacon && \
-echo 'anacon_pwd' | passwd --stdin anacon && \
-echo 'anacon ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-echo 'root_pwd' | passwd --stdin root
--- centos-7-6-18-10-install-ghc-haskell-vim-env-usr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-groupadd -g 1001 haskell && \
-useradd -m -g haskell -u 1001 haskell && \
-chsh -s /bin/bash haskell && \
-echo 'haskell_pwd' | passwd --stdin haskell && \
-echo 'haskell ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-echo 'root_pwd' | passwd --stdin root
