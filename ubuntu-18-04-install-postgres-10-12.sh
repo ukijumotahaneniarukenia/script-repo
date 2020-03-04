c605ff2ec53291104abd5dbf227fe626f249df0d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-04 21:22:37 +0900

    repo

--- ubuntu-18-04-install-postgres-10-12.sh
+++ ubuntu-18-04-install-postgres-10-12.sh
@@ -4,6 +4,6 @@ curl -LO https://ftp.postgresql.org/pub/source/v10.12/postgresql-10.12.tar.gz &&
 tar xvf postgresql-10.12.tar.gz && \
 chown -R root:root postgresql-10.12 && \
 cd postgresql-10.12 && \
-./configure --prefix=/usr/local && \
+./configure --prefix=/usr/local  --libdir=/usr/local/lib/postgresql && \
 make -j12 && \
 make -j12 install
