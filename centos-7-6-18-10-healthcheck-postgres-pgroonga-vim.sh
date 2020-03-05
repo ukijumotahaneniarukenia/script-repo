e711eb40b12fe6111d103257d10f7ffb89664476
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-06 07:46:52 +0900

    repo

--- centos-7-6-18-10-healthcheck-postgres-pgroonga-vim.sh
+++ centos-7-6-18-10-healthcheck-postgres-pgroonga-vim.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-
 su - postgres <<EOF
 ps aux | grep -P '/usr/local/bin/postgres -D /home/postgres/dat' | grep -v grep | awk '{print \$2}' | xargs kill -9
 rm -rf /home/postgres/dat
 initdb -D /home/postgres/dat
 pg_ctl -D /home/postgres/dat -l /home/postgres/launch-postgres.log start
+
+sleep 100
+
 psql -U postgres -c "create database testdb"
 psql -U postgres -d testdb -c "SELECT * FROM pg_available_extensions;"
 psql -U postgres -d testdb -c "CREATE EXTENSION pgroonga;"
