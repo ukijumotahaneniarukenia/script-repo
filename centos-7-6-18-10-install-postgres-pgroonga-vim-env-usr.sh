56597c5ee4d1384277780dcde8b54684f3ecc133
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-05 21:42:14 +0900

    repo

---
+++ centos-7-6-18-10-install-postgres-pgroonga-vim-env-usr.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+groupadd -g 1001 postgres && \
+useradd -m -g postgres -u 1001 postgres && \
+chsh -s /bin/bash postgres && \
+echo 'postgres_pwd' | passwd --stdin postgres && \
+echo 'postgres ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo 'root_pwd' | passwd --stdin root
--- centos-7-6-18-10-launch-postgres-12-0.sh
+++ centos-7-6-18-10-launch-postgres-12-0.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-MX_USER_ID=$(cat /etc/passwd | grep -Po '[0-9]+(?=:)' | sort | tail -n1)
-
-groupadd -g $((MX_USER_ID+1)) postgres && \
-useradd -m -g postgres -u $((MX_USER_ID+1)) postgres && \
-chsh -s /bin/bash postgres && \
-echo 'postgres_pwd' | passwd --stdin postgres && \
-echo 'postgres ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
-echo 'root_pwd' | passwd --stdin root
-
 su - postgres <<EOF
 
 ps aux | grep -P '/usr/local/bin/postgres -D /home/postgres/dat' | grep -v grep | awk '{print \$2}' | xargs kill -9
