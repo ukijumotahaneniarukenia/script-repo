84eef6fde008e2b527ef2623d0680ac5286db3e1
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-03-07 16:11:34 +0900

    repo

--- ubuntu-16-04-install-mysql-5-7-29-mroonga-9-1-2.sh
+++ ubuntu-16-04-install-mysql-5-7-29-mroonga-9-1-2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-apt install -y -V software-properties-common lsb-release
-add-apt-repository -y universe
-add-apt-repository "deb http://security.ubuntu.com/ubuntu $(lsb_release --short --codename)-security main restricted"
-add-apt-repository -y ppa:groonga/ppa
-apt update
-apt install -y -V mysql-server-mroonga
-apt install -y -V groonga-tokenizer-mecab
+
+groupadd -g 1001 mysql && \
+useradd -m -g mysql -u 1001 mysql && \
+chsh -s /bin/bash mysql && \
+echo 'mysql:mysql_pwd' | chpasswd && \
+echo 'mysql ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo 'root:root_pwd' | chpasswd
