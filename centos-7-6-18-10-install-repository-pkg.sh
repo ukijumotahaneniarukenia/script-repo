a780acf350bb63ce559a4652952da5e4db268be6
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-03-08 20:02:59 +0900

    Update centos-7-6-18-10-install-repository-pkg.sh

--- centos-7-6-18-10-install-repository-pkg.sh
+++ centos-7-6-18-10-install-repository-pkg.sh
@@ -2,5 +2,8 @@
 yum install -y \
 epel-release
 
+rpm --import http://mirror.centos.org/centos/RPM-GPG-KEY-CentOS-7
+rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
+
 yum update -y
 yum upgrade -y
