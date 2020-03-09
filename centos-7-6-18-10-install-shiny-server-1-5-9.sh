c0dfa5baf91b64e0476f84d522db1e8ca8601046
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-03-10 08:29:21 +0900

    Update centos-7-6-18-10-install-shiny-server-1-5-9.sh

--- centos-7-6-18-10-install-shiny-server-1-5-9.sh
+++ centos-7-6-18-10-install-shiny-server-1-5-9.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /usr/local/src && \
 curl -LO https://download3.rstudio.org/centos6.3/x86_64/shiny-server-1.5.9.923-x86_64.rpm && \
-yum install -y --nogpgcheck shiny-server-1.5.9.923-x86_64.rpm
+yum install -y shiny-server-1.5.9.923-x86_64.rpm
 
 R -e 'install.packages("rmarkdown",repos="http://cran.ism.ac.jp/" ,dependencies = TRUE, Ncpus = getOption("Ncpus", 12L))'
 R -e 'install.packages("shiny",repos="http://cran.ism.ac.jp/" ,dependencies = TRUE, Ncpus = getOption("Ncpus", 12L))'
