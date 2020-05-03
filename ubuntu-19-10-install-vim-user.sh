f179975992c51bc2ed4ccebe10b3c8fd9cd73a41
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-04 03:12:03 +0900

    sketch

--- ubuntu-19-10-install-vim-user.sh
+++ ubuntu-19-10-install-vim-user.sh
@@ -9,6 +9,3 @@ cd $HOME && \
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && \
 sh installer.sh $HOME/.vim/bundle && \
 rm $HOME/installer.sh
-echo 'root:root_pwd' | chpasswd
-echo 'root:root_pwd' | chpasswd
-echo 'root:root_pwd' | chpasswd
