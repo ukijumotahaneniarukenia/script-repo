5fb7158988e312c17402d2b6f97104c88d994d81
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-04 00:24:40 +0900

    Update centos-7-6-18-10-install-vim-user.sh

--- centos-7-6-18-10-install-vim-user.sh
+++ centos-7-6-18-10-install-vim-user.sh
@@ -9,6 +9,3 @@ cd $HOME && \
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && \
 sh installer.sh $HOME/.vim/bundle && \
 rm $HOME/installer.sh
-echo 'root_pwd' | passwd --stdin root
-echo 'root_pwd' | passwd --stdin root
-echo 'root_pwd' | passwd --stdin root
