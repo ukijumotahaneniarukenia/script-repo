8a62d8ed5e84ec6de167e432c53ce25361e8c10f
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-04 02:30:36 +0900

    Create ubuntu-19-10-install-kdevelop-5-5-0.sh

---
+++ ubuntu-19-10-install-kdevelop-5-5-0.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd /usr/local/src
+wget -O KDevelop.AppImage https://download.kde.org/stable/kdevelop/5.5.0/bin/linux/KDevelop-5.5.0-x86_64.AppImage
+chmod +x KDevelop.AppImage
+mv KDevelop.AppImage /usr/local/bin
+which KDevelop.AppImage
+apt install fuse
+groupadd fuse
+usermod -a -G fuse kuraine #自動化を視野に入れるとこの行が融通聴いていないけどいまはこれでいい
