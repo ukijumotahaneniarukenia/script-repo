fcdc521885e02e18d390eadd6f67fe29a3d6b987
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-04 00:09:49 +0900

    Update centos-7-6-18-10-install-kibana-7-5-1.sh

--- centos-7-6-18-10-install-kibana-7-5-1.sh
+++ centos-7-6-18-10-install-kibana-7-5-1.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 cd /usr/local/src && \
 curl -LO https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-x86_64.rpm && \
-yum install -y kibana-7.5.1-x86_64.rpm && \
-sed -i 's;#server.port: 5601;server.port: 5601;' /etc/kibana/kibana.yml && \
-( IP=$(ifconfig eth0 | grep inet | sed -E 's;\s{1,}; ;' | cut -d" " -f 3) && sed -i "s;#server.host: \"localhost\";server.host: \"$IP\";" /etc/kibana/kibana.yml )
+yum install -y kibana-7.5.1-x86_64.rpm
