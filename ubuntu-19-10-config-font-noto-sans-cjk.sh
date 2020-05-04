8542b2d9fa114300a880ee08801c27a039ad2ba9
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-04 15:29:52 +0900

    Update ubuntu-19-10-config-font-noto-sans-cjk.sh

--- ubuntu-19-10-config-font-noto-sans-cjk.sh
+++ ubuntu-19-10-config-font-noto-sans-cjk.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-mkdir ~/.fonts/noto-sans-cjk
-cd ~/.fonts/noto-sans-cjk
+mkdir -p $HOME/.fonts && \
+cd $HOME/.fonts && \
 wget https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
 unzip NotoSansCJKjp-hinted.zip
-cd ~/.fonts/
-mv ~/.fonts/noto-sans-cjk/*otf .
+mv *otf .
 fc-cache -fv
