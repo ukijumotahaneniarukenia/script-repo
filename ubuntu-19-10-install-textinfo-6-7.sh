2780c435cd259b53666195d7d479d5fdfe70e688
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-04 18:16:29 +0900

    Create ubuntu-19-10-install-textinfo-6-7.sh

---
+++ ubuntu-19-10-install-textinfo-6-7.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd /usr/local/src
+curl -sSLO https://ftp.gnu.org/gnu/texinfo/texinfo-6.7.tar.gz
+tar xvf texinfo-6.7.tar.gz
+cd texinfo-6.7
+./configure
+make -j12
+make -j12 install
+which makeinfo #makeinfoコマンドはtextinfoソースに梱包
