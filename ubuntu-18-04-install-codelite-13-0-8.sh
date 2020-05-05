285c5edc400d01c69898fa79c165fcf8dede6190
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-05 15:03:30 +0900

    sketch

---
+++ ubuntu-18-04-install-codelite-13-0-8.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /usr/local/src
+curl -sSLO https://downloads.codelite.org/codelite/13.0.8/CodeLite-13.0.8-gtk3-ubuntu-bionic-x86_64.deb
+dpkg -i CodeLite-13.0.8-gtk3-ubuntu-bionic-x86_64.deb
