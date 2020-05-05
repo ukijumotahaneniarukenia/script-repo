ccc127ba94a38683bc3fd4bf5f146fcaa18f46de
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-05-05 22:30:05 +0900

    Update centos-7-6-18-10-install-postgres-12-0-with-python.sh

--- centos-7-6-18-10-install-postgres-12-0-with-python.sh
+++ centos-7-6-18-10-install-postgres-12-0-with-python.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 
-yum install -y python-devel
+#ソースからビルドうまくいかんので、レポからいんすこ
 
-cd /usr/local/src && \
-curl -LO https://ftp.postgresql.org/pub/source/v12.0/postgresql-12.0.tar.gz && \
-tar xvf postgresql-12.0.tar.gz && \
-chown -R root:root postgresql-12.0 && \
-cd postgresql-12.0 && \
-./configure --prefix=/usr/local --libdir=/usr/local/lib/postgresql --with-python && \
-make -j12 && \
-make -j12 install
+#yum install -y python-devel
+
+#cd /usr/local/src && \
+#curl -LO https://ftp.postgresql.org/pub/source/v12.0/postgresql-12.0.tar.gz && \
+#tar xvf postgresql-12.0.tar.gz && \
+#chown -R root:root postgresql-12.0 && \
+#cd postgresql-12.0 && \
+#./configure --prefix=/usr/local --libdir=/usr/local/lib/postgresql --with-python && \
+#make -j12 && \
+#make -j12 install
+
+yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+yum -y install postgresql12-server postgresql12-devel postgresql12-libs postgresql12-plpython3
+
+find / -name "*Python.h*" 2>/dev/null #ヘッダファイルの確認
+
+find / -name "*libpython*so*" 2>/dev/null #pythonダイナミックリンクライブラリの確認
+
+find / -name "*plpython*so*" 2>/dev/null #plpythonダイナミックリンクライブラリの確認
+
+ldconfig -p | grep py #キャッシュできているか確認
+
+ldd /usr/pgsql-12/lib/plpython3.so #紐づき確認
+
+cat <<EOS >/home/postgres/.pgsql_profile
+PATH=/usr/pgsql-12/bin:$PATH
+MANPATH=/usr/pgsql-12/share/man:$MANPATH
+PGDATA=/var/lib/pgsql/12/data
+export PATH MANPATH PGDATA
+EOS
+
+source /home/postgres/.pgsql_profile
