f2728d4e967d8777ce4448f9fb7e0813005a8723
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-05 20:10:26 +0900

    sketch

--- ubuntu-16-04-install-postgres-12-0-with-python.sh
+++ ubuntu-16-04-install-postgres-12-0-with-python.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-apt install -y python-devel
+#apt install -y python-devel
+#ダイナミックリンクライブラリの整備
+echo "export LD_LIBRARY_PATH=/usr/local/lib" >> ~/.bashrc && source ~/.bashrc
+echo '/usr/local/lib'>>/etc/ld.so.conf.d/local-ld.so.conf
+ldconfig
+
+#ライブラリキャッシュが更新されたか確認
+ldconfig -p | grep py
+
+#ヘッダファイル確認
+find / -name "*Python.h*" 2>/dev/null
 
 cd /usr/local/src && \
 curl -LO https://ftp.postgresql.org/pub/source/v12.0/postgresql-12.0.tar.gz && \
