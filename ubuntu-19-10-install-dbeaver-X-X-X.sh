b3eaa37d8702d1b59ee54b660e00b084075eefe0
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-07 00:38:46 +0900

    rep

--- ubuntu-19-10-install-dbeaver-X-X-X.sh
+++ ubuntu-19-10-install-dbeaver-X-X-X.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+apt install openjdk-8-jre-headless ca-certificates-java java-common libpcsclite1
+
 cd /usr/local/src && \
 curl -LO https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb && \
 dpkg -i dbeaver-ce_latest_amd64.deb
