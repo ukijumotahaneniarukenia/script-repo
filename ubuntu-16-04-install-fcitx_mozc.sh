2575eae5d3492b08a19ee4ed02295341eab24457
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-07 18:39:22 +0900

    repo

---
+++ ubuntu-16-04-install-fcitx_mozc.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#レポジトリの設定修正
+cp -p /etc/apt/sources.list /etc/apt/sources.list.bak
+sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+apt update
+
+#インプットメソッドのインストール
+apt install -y fcitx-mozc
+
+#dbus-deamonコマンドのインストール
+apt install -y dbus-x11
+
+#レポジトリの設定修正
+cp -p /etc/apt/sources.list.bak /etc/apt/sources.list
+apt update
