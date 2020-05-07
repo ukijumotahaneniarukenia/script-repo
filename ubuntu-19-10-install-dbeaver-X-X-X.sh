641ce95036159cf9eff1de7679072ac3c82da8ea
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-08 00:59:44 +0900

    sketch

--- ubuntu-19-10-install-dbeaver-X-X-X.sh
+++ ubuntu-19-10-install-dbeaver-X-X-X.sh
@@ -7,3 +7,14 @@ apt install -y libswt-webkit-gtk-4-jni
 cd /usr/local/src && \
 curl -LO https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb && \
 dpkg -i dbeaver-ce_latest_amd64.deb
+
+#ubuntu系のdbeaverのインストール方法（別のやり方）
+#
+#apt install -y openjdk-11-jdk openjdk-11-jre
+#java -version
+#wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | apt-key add -
+#echo "deb https://dbeaver.io/debs/dbeaver-ce /" | tee /etc/apt/sources.list.d/dbeaver.list
+#apt update
+#apt install -y dbeaver-ce
+#apt-cache search swt
+#apt install -y libswt-webkit-gtk-4-jni
