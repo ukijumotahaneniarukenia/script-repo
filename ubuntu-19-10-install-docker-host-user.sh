950932ec44e4db824dc8e136be39ab57705519d2
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-05-13 09:24:14 +0900

    sketch

--- ubuntu-19-10-install-docker-host-user.sh
+++ ubuntu-19-10-install-docker-host-user.sh
@@ -2,6 +2,5 @@
 groupadd -g 1000 aine && \
 useradd -m -g aine -u 1000 aine && \
 chsh -s /bin/bash aine && \
-echo 'aine_pwd' | passwd --stdin aine && \
-echo 'aine ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
-echo 'root_pwd' | passwd --stdin root
+echo 'aine:aine_pwd' | chpasswd && \
+echo 'aine ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
