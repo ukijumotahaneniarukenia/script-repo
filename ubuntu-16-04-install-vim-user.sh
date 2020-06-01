20d9765d024a20973b2bf544933b028e7c15cfa0
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-01 14:05:33 +0900

    repo

--- ubuntu-16-04-install-vim-user.sh
+++ ubuntu-16-04-install-vim-user.sh
@@ -4,16 +4,3 @@
 rm -rf $HOME/.vim
 
 git clone https://github.com/ukijumotahaneniarukenia/.vim.git $HOME/.vim
-
-#vim-plugin-manager
-rm -rf $HOME/.vim/bundle
-
-mkdir -p $HOME/.vim/bundle
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $HOME/installer.sh
-
-
-sh $HOME/installer.sh $HOME/.vim/bundle
-
-
-rm -rf $HOME/installer.sh
