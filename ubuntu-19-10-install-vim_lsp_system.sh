e7c96695a495da826b4b45b5ccb348046e99f2aa
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-03 15:15:17 +0900

    repo

---
+++ ubuntu-19-10-install-vim_lsp_system.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#https://github.com/mattn/vim-lsp-settings
+
+#vimコード補完拡張
+npm install -g vim-language-server
+
+#pythonコード補完拡張
+pip3 install --user python-language-server
+
---
+++ ubuntu-19-10-install-vim_lsp_user.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#https://github.com/mattn/vim-lsp-settings
+
+#pythonコード補完拡張
+pip3 install --user python-language-server
+
