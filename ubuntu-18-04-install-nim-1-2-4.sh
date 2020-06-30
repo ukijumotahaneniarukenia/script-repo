e5c4ee8be85c69340f98e2830cb9768c4f4865ad
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-06-30 11:49:01 +0900

    repo

---
+++ ubuntu-18-04-install-nim-1-2-4.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src
+
+curl -fsSLO https://nim-lang.org/download/nim-1.2.4-linux_x64.tar.xz
+
+tar xvf nim-1.2.4-linux_x64.tar.xz
+
+cd nim-1.2.4
+
+./install.sh /usr/local/bin
+
+./koch nimble
+
+ln -fsr /usr/local/src/nim-1.2.4/bin/nimble /usr/local/bin/nimble
--- ubuntu-19-10-install-nim-1-0-6.sh
+++ ubuntu-19-10-install-nim-1-0-6.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-cd /usr/local/src && \
-curl -LO https://nim-lang.org/download/nim-1.0.6-linux_x64.tar.xz && \
-tar xvf nim-1.0.6-linux_x64.tar.xz && \
-cd nim-1.0.6 && \
-./install.sh /usr/local/bin && \
-./koch nimble && \
+cd /usr/local/src
+
+curl -LO https://nim-lang.org/download/nim-1.0.6-linux_x64.tar.xz
+
+tar xvf nim-1.0.6-linux_x64.tar.xz
+
+cd nim-1.0.6
+
+./install.sh /usr/local/bin
+
+./koch nimble
+
 ln -fsr /usr/local/src/nim-1.0.6/bin/nimble /usr/local/bin/nimble
