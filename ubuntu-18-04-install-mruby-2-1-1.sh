91d4f11097754b67c4aada082703c63ad47b8503
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-02 00:30:28 +0900

    repo

--- ubuntu-18-04-install-mruby-2-1-1.sh
+++ ubuntu-18-04-install-mruby-2-1-1.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
-cd /usr/local/src && \
-curl -LO http://ftp.yz.yamagata-u.ac.jp/pub/network/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz && \
-tar xvf apache-maven-3.6.3-bin.tar.gz
+
+
+#https://github.com/mruby/mruby/releases/tag/2.1.1
+curl -fsSL https://github.com/mruby/mruby/archive/2.1.1.tar.gz -o mruby-2.1.1.tar.gz
+
+
+tar xvf mruby-2.1.1.tar.gz
+
+
+cd mruby-2.1.1
+
+
+apt install -y bison
+
+make -j12
+
+
+find -type f | xargs file | grep shared | grep -v build | cut -d ':' -f 1
+
+find -type f | xargs file | grep shared | grep -v build | cut -d ':' -f 1 | xargs -I@ mv @ /usr/local/bin/
+
+
+mruby --version
+
+mrbc --version
+
+mirb --version
+
+mrdb --version
+
+mruby-strip --version
