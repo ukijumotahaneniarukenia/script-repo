e639c452e08ff448802ae4241f66009d659059f8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-01 23:06:52 +0900

    repo

---
+++ ubuntu-18-04-install-mruby_cli-0-0-4.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl -fsSLO https://github.com/hone/mruby-cli/releases/download/v0.0.4/mruby-cli-0.0.4-x86_64-pc-linux-gnu.tgz
+
+tar xvf mruby-cli-0.0.4-x86_64-pc-linux-gnu.tgz
+
+mv mruby-cli /usr/local/bin/mruby-cli
+
+mruby-cli --version
