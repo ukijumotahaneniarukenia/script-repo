1ba1fea23e70e6e6870950db2bc6eea5afa23144
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-08 03:47:49 +0900

    repo

---
+++ ubuntu-18-04-install-w3m.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#https://github.com/tats/w3m/
+
+
+#マニュアル
+#find | grep MAN
+#./doc/MANUAL.html
+#./doc-jp/MANUAL.html
+#./doc-de/MANUAL.html
+#
+#firefox ./doc-jp/MANUAL.html
+
+
+apt install -y w3m
