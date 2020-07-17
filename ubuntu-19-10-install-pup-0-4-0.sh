c38e765e362c1a39db0163af3021872e831d2958
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-18 03:24:54 +0900

    repo

---
+++ ubuntu-19-10-install-pup-0-4-0.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src/
+
+curl -sSLO https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip
+
+unzip pup_v0.4.0_linux_amd64.zip
+
+mv pup /usr/local/bin/
