0c5afe583408cc7b88f90cc034fc6fef17ab7b40
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-24 00:32:27 +0900

    repo

---
+++ ubuntu-19-10-install-dart-2-8-4.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd /usr/local/src
+
+apt-get update
+
+apt-get install -y apt-transport-https
+
+
+sh -c 'wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O - | apt-key add -'
+
+sh -c 'wget -q https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list -O - > /etc/apt/sources.list.d/dart_stable.list'
+
+apt-get update
+
+which dart
+#/usr/bin/dart
+
+dart --version
+#Dart VM version: 2.8.4 (stable) (Unknown timestamp) on "linux_x64"
