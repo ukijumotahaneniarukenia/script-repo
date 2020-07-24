27a378be6a56435ce06de75da07bbffae0ac3caf
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-25 03:50:59 +0900

    repo

---
+++ ubuntu-19-10-install-vivaldi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src
+
+apt install -y fonts-liberation libappindicator3-1 libindicator3-7
+
+
+dpkg -i vivaldi-stable_3.1.1929.45-1_amd64.deb
+
+
+which vivaldi
+#/usr/bin/vivaldi
+
