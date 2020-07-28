aeb5f707c8122783d86c6855e5357664b4dec30c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-07-29 05:46:59 +0900

    repo

--- centos-7-7-19-18-install-default-user.sh
+++ centos-7-7-19-18-install-default-user.sh
@@ -4,11 +4,13 @@ DEFAULT_USER_ID=9999
 DEFAULT_USER_NAME=kuraine
 DEFAULT_GROUP_ID=9999
 DEFAULT_GROUP_NAME=kuraine
+DEFAULT_PASSWORD=kuraine_pwd
 
 USER_ID=$1;shift;
 USER_NAME=$1;shift;
 GROUP_ID=$1;shift;
 GROUP_NAME=$1;shift;
+PASSWORD=$1;shift;
 
 if [ -z $USER_ID ];then
   :
@@ -34,9 +36,15 @@ else
   DEFAULT_GROUP_NAME=$GROUP_NAME
 fi
 
+if [ -z $PASSWORD ];then
+  :
+else
+  DEFAULT_PASSWORD=$PASSWORD
+fi
+
 groupadd -g $DEFAULT_GROUP_ID $DEFAULT_GROUP_NAME && \
 useradd -m -g $DEFAULT_GROUP_NAME -u $DEFAULT_USER_ID $DEFAULT_USER_NAME && \
 chsh -s /bin/bash $DEFAULT_USER_NAME && \
-echo $DEFAULT_USER_NAME'_pwd' | passwd --stdin $DEFAULT_USER_NAME && \
+echo $DEFAULT_PASSWORD | passwd --stdin $DEFAULT_USER_NAME && \
 echo "$DEFAULT_USER_NAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
 echo 'root_pwd' | passwd --stdin root
