b8c7910a5bf09aa031708200277b34801ee632f1
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-10 19:07:12 +0900

    repo

--- ubuntu-18-04-install-golang-1-13-5.sh
+++ ubuntu-18-04-install-golang-1-13-5.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-cd /usr/local/src && curl -LO https://dl.google.com/go/go1.4.3.linux-amd64.tar.gz && \
-cd /usr/local/src && curl -LO https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz && \
-cd /usr/local/src && mkdir -p go1.4 && \
-cd /usr/local/src && tar xvf go1.4.3.linux-amd64.tar.gz && \
-cd /usr/local/src && mv /usr/local/src/go/* go1.4 && \
-cd /usr/local/src && tar xvf go1.13.5.linux-amd64.tar.gz && \
-cd go/src && export GOROOT_BOOTSTRAP=/usr/local/src/go1.4 && ./all.bash
+cd /usr/local/src
+
+curl -fsSLO https://dl.google.com/go/go1.4.3.linux-amd64.tar.gz
+
+curl -fsSLO https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+
+
+
+#ブートストラップ用のgolangを設定
+mkdir -p /usr/local/src/go1.4
+tar xvf go1.4.3.linux-amd64.tar.gz
+mv /usr/local/src/go/* /usr/local/src/go1.4
+
+
+tar xvf go1.13.5.linux-amd64.tar.gz
+cd /usr/local/src/go/src
+export GOROOT_BOOTSTRAP=/usr/local/src/go1.4
+./all.bash
