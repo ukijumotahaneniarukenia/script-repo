6490574cc51dd8eca7fce3d0eff29c0c1b35adbc
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-10 15:13:02 +0900

    repo

---
+++ ubuntu-18-04-install-nvim_plug.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -rf $HOME/.local/share/nvim/site/autoload
+
+#https://github.com/junegunn/vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
