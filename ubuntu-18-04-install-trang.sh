19db151d66f22c3ec8d9dbb4bdb39e9477b8ce83
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-14 13:19:34 +0900

    repo

---
+++ ubuntu-18-04-install-trang.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#https://github.com/relaxng/jing-trang/releases/tag/V20181222
+
+cd /usr/local/src
+
+curl -fsSLO https://github.com/relaxng/jing-trang/releases/download/V20181222/trang-20181222.zip
