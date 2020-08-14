526d969be25295cab2729543a787b16272a66644
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-14 13:41:12 +0900

    repo

--- ubuntu-18-04-install-jaxb_ri-2-3-1.sh
+++ ubuntu-18-04-install-jaxb_ri-2-3-1.sh
@@ -7,3 +7,7 @@ cd /usr/local/src
 curl -fsSLO https://repo1.maven.org/maven2/com/sun/xml/bind/jaxb-ri/2.3.1/jaxb-ri-2.3.1.zip
 
 unzip jaxb-ri-2.3.1.zip
+
+ln -fsr /usr/local/src/jaxb-ri/bin/xjc.sh /usr/local/src/jaxb-ri/bin/xjc
+
+ln -fsr /usr/local/src/jaxb-ri/bin/schemagen.sh /usr/local/src/jaxb-ri/bin/schemagen
