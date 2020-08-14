901efb65d735cff23683ce3f75d372b154457da9
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-14 14:21:10 +0900

    repo

--- ubuntu-18-04-install-jaxb_ri-2-3-1.sh
+++ ubuntu-18-04-install-jaxb_ri-2-3-1.sh
@@ -11,3 +11,17 @@ unzip jaxb-ri-2.3.1.zip
 ln -fsr /usr/local/src/jaxb-ri/bin/xjc.sh /usr/local/src/jaxb-ri/bin/xjc
 
 ln -fsr /usr/local/src/jaxb-ri/bin/schemagen.sh /usr/local/src/jaxb-ri/bin/schemagen
+
+
+#このままだと以下のエラーがでて使えなかったので、
+
+#Error occurred during initialization of boot layer
+#java.lang.module.FindException: Module java.activation not found, required by java.xml.bind
+
+#これを追加しておく
+#https://mvnrepository.com/artifact/com.sun.activation/javax.activation/1.2.0
+
+#似たような案件
+#https://qiita.com/Targityen/items/08d2846c5b6639e805ef
+cd /usr/local/src/jaxb-ri/mod
+curl -sSLO https://repo1.maven.org/maven2/com/sun/activation/javax.activation/1.2.0/javax.activation-1.2.0.jar
