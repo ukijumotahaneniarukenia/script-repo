e1d14ae0093459fa4ec23e05a7a3aac74abacd52
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-15 01:39:42 +0900

    repo

---
+++ ubuntu-20-04-install-p4merge.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#https://www.perforce.com/products/helix-core-apps/merge-diff-tool-p4merge
+#ユーザー登録はしなくて大丈夫
+cd /usr/local/src
+
+tar xvf p4v.tgz
+
+which p4merge
+#/usr/local/src/p4v-2020.1.1966006/bin/p4merge
