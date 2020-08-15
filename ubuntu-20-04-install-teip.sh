b4aeff9ce96d47ae332877e5d1abc6998d7e6316
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-15 20:31:00 +0900

    repo

---
+++ ubuntu-20-04-install-teip.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src
+
+curl -sfSL --retry 5 https://git.io/teip-1.2.0.x86_64.deb -o teip.deb
+
+dpkg -i teip.deb
+
+
+#$ teip
+#teip: Invalid arguments.
+#
+#Usage:
+#  teip -g <pattern> [-oGsvz] [--] [<command>...]
+#  teip -f <list> [-d <delimiter> | -D <pattern>] [-svz] [--] [<command>...]
+#  teip -c <list> [-svz] [--] [<command>...]
+#  teip -l <list> [-svz] [--] [<command>...]
+#  teip --help | --version
