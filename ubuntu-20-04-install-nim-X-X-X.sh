f5a637fdd5ad8efaa6edbde66fbbdc17e6a32eb6
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-08-17 01:34:05 +0900

    repo

---
+++ ubuntu-20-04-install-nim-X-X-X.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+#たまにコケるときは以下のファイルを削除するなど
+#sudo rm /tmp/nimblecache/nimscriptapi.nim
+#sudo rm /tmp/choosenim-0.6.0_linux_amd64
+
+#実行ユーザー単位でインストールする方法に切り替え
+#プロンプト聞かれるので、yと答える
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+
+choosenim stable
+
+#プロンプトあるので、yesと答える
+#https://github.com/PMunch/nimlsp
+nimble install nimlsp
