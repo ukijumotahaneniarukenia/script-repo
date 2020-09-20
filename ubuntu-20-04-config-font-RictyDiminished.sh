af09c4ce56efccc6442205af5261d30313678e89
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-20 16:03:44 +0900

    repo

--- ubuntu-20-04-config-font-RictyDiminished.sh
+++ ubuntu-20-04-config-font-RictyDiminished.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
-apt install -y fontconfig
-mkdir -p $HOME/.fonts && \
-cd $HOME/.fonts && \
+
+if [ $UID -eq 0 ];then
+
+  apt install -y fontconfig
+
+fi
+
+mkdir -p $HOME/.fonts
+
+cd $HOME/.fonts
+
+rm -rf master.zip
+
+rm -rf RictyDiminished-for-Powerline-master
+
 curl -sSLO https://github.com/mzyy94/RictyDiminished-for-Powerline/archive/master.zip
+
 unzip master.zip
+
 mv RictyDiminished-for-Powerline-master/*ttf .
+
 fc-cache -fv
