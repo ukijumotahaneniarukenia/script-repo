4d17e4eecab5b757ef66256bd13ee232732eb40a
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-27 10:58:52 +0900

    repo

--- ubuntu-20-04-install-qt_designer.sh
+++ ubuntu-20-04-install-qt_designer.sh
@@ -2,6 +2,10 @@
 
 cd /usr/local/src
 
+apt install -y qttools5-dev-tools qttools5-dev
+
+apt install -y python3-qtpy pyqt5-dev pyqt5-dev-tools python3-pyqt5 libqt5widgets5 libqt5gui5 libqt5dbus5 libqt5network5 libqt5core5a
+
 pip3 install --upgrade pip
 
 pip3 install SIP
