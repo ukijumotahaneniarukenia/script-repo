fdc3166a336a83cf67e6be8fe1237ae987a8d40b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-10-22 09:39:17 +0900

    sketch

---
+++ ubuntu-18-04-install-fuego-0-11-0.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd /usr/local/src/
+
+curl -fsSLO https://github.com/sgarciac/fuego/releases/download/v0.11.0/fuego_0.11.0_Linux_64-bit.tar.gz
+
+mkdir -p /usr/local/src/fuego-bin
+
+tar xvf fuego_0.11.0_Linux_64-bit.tar.gz -C /usr/local/src/fuego-bin
+
+which fuego
