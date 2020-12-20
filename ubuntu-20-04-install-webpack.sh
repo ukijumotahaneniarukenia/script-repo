09d0db128215f232a3135e617c8c01e843da598d
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-20 16:01:14 +0900

    repo

--- ubuntu-20-04-install-webpack.sh
+++ ubuntu-20-04-install-webpack.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 apt install -y webpack
+
+
+#$ file /usr/bin/webpack
+#/usr/bin/webpack: symbolic link to ../share/nodejs/webpack/bin/webpack.js
+#
+#$ readlink -f /usr/bin/webpack
+#/usr/share/nodejs/webpack/bin/webpack.js
+
