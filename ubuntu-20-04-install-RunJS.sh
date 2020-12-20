cddac6a82d57543f5aad05f735af0d1b9132acb8
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-12-20 15:57:56 +0900

    repo

--- ubuntu-20-04-install-RunJS.sh
+++ ubuntu-20-04-install-RunJS.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-
 #https://runjs.dev/
 
 cd /usr/local/src
 
-cp /home/aine/Downloads/RunJS-1.12.2.AppImage .
+mkdir -p /usr/local/src/RunJS
 
-chmod 755 ./RunJS-1.12.2.AppImage
+cp /home/aine/Downloads/RunJS-1.12.2.AppImage /usr/local/src/RunJS/RunJS-1.12.2.AppImage
+
+chmod 755 /usr/local/src/RunJS/RunJS-1.12.2.AppImage
