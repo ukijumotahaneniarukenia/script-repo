9f20bd78090f82472558fcce8ff04daec1c7eb8b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-02-22 09:05:37 +0900

    repo

--- ubuntu-19-10-install-vim-user.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#.vim
-rm -rf $HOME/.vim
-
-git clone https://github.com/ukijumotahaneniarukenia/.vim.git $HOME/.vim
--- ubuntu-19-10-install-vim_plug.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf $HOME/.vim/autoload
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
