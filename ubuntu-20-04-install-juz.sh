5fef72fc3e3c812e2b0b222a53977d31ede3c93c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2021-04-24 21:08:09 +0900

    repo

---
+++ ubuntu-20-04-install-juz.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#https://github.com/ryuichiueda/glueutils
+
+cd /usr/local/src
+
+git clone https://github.com/ryuichiueda/glueutils.git
+
+
+cd glueutils
+
+
+make install -j12
